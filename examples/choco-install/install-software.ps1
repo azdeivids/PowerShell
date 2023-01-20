72050bdd19342d73ec7db54f3379949b9533f45c
deivids <deivids.egle@gmail.com>
2023-01-20 14:49:27 +0000

    added unifi tool for donwload

--- examples/choco-install/install-software.ps1
+++ examples/choco-install/install-software.ps1
@@ -1,5 +1,5 @@
 $URI = "https://raw.githubusercontent.com/azdeivids/PowerShell/main/examples/choco-install/ent_packages.config"
-$CONFIG = ".\packages.config"
+$CONFIG = "C:\Users\degle\Dev\PowerShell\examples\choco-install\ent_packages.config"
 #Requires -RunAsAdministrator
 $run_local=$args[0]
 
@@ -10,7 +10,7 @@ if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
 
 if ($run_local -eq "local") {
 # for each package in the list run install
-  Get-Content $CONFIG | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install -y $_}
+  Get-Content "C:\Users\degle\Dev\PowerShell\examples\choco-install\ent_packages.config" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install -y $_}
 }
 else
 {
@@ -18,3 +18,15 @@ else
 Write-Host "File not found!!!"  
 (Invoke-webrequest -URI $URI).Content | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install -y $_}
 }
+
+Start-Sleep -Seconds 360
+
+$UnifiSearchToolUrl = "https://github.com/Crypto-Spartan/unifi-search-tool/releases/download/2.0.1/unifi-search-tool_v2.0.1.exe"
+$UnifiSeachToolOutpath = "C:\temp\unifi-search-tool_v2.0.1.exe"
+  
+  Invoke-WebRequest -Uri $UnifiSearchToolUrl -OutFile $UnifiSeachToolOutpath
+
+  C:\temp\unifi-search-tool_v2.0.1.exe
+
+Install-Module -Name Terminal-Icons -Repository PSGallery
+Install-Module posh-git
